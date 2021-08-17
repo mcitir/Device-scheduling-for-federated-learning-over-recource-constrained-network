36f162e281960d1ad5a9097637d76cc14d8b635e
Jarrlist <jarrlist@gmail.com>
2021-08-17 15:36:09 +0200

    Bagun work on large scale fading

--- test/test_channel.py
+++ test/test_channel.py
@@ -4,14 +4,25 @@ script_dir = os.path.dirname( __file__ )
 mymodule_dir = os.path.join( script_dir, '..', 'utils')
 sys.path.append( mymodule_dir)
 import channel
-
+import matplotlib.pyplot as plt
 
 num_users = 100
-std_noise = 1
-power = 0.1
-B = 0
-beta = 1
-cg = channel.channel_capacity(num_users, std_noise, power, B, beta)
+noise_power = 0.01
+power = 1
+B = 10**5
+cg = channel.channel_capacity(num_users, noise_power, power, B)
+
 
 print("Testing")
-print(cg)
+
+x = channel.hexagon(num_users)
+#print(x)
+#plt.scatter(x[0], x[1])
+#plt.show()
+
+y = channel.distances(1000*num_users)
+print(y)
+plt.hist(y, bins=200)
+plt.show()
+
+# print(cg)
--- utils/channel.py
+++ utils/channel.py
@@ -1,7 +1,7 @@
-from numpy import log2
-from numpy.random import randn, random, standard_normal
+from numpy import log2, sqrt, zeros, transpose
+from numpy.random import randn, random, standard_normal, uniform
 
-def channel_capacity(num_users, std_noise, power, B, beta):
+def channel_capacity(num_users, noise_power, power, B):
     """
     num_users: number of users
 
@@ -11,19 +11,48 @@ def channel_capacity(num_users, std_noise, power, B, beta):
 
     B: Bandwidth
 
-    beta: variance of channel gain
+    beta: large scale fading coefficent
 
     """
     # Channel gain, based on relight fading, rich multipath
-
-    # g = CN(0, beta)
-    g = ((standard_normal(num_users) + 1j * standard_normal(num_users)) * 
-        beta * 0.5)
-
+    beta = 1
+    # h = CN(0, 1), a.k.a relight fading
+    h = (standard_normal(num_users) + 1j * standard_normal(num_users)) * 1 * 0.5
+    g = sqrt(beta)*h
     # capacity = log2(1+|g|^2 SNR)
     # SNR = q/N0
     # std_noise^2 = No/2 
-    cg = log2( (1 + abs(g)**2 * power/(std_noise^2)))
+    cg = log2(1 + (abs(g)**2 * power/(noise_power)))
 
     return cg
 
+def hexagon(nbr_users, size=500):
+    x = zeros((2, nbr_users))
+    for i in range(nbr_users):
+        r = uniform(0,1)
+        uv = [uniform(0,1), uniform(0,1)]
+
+        if r <= 1/3:
+            x[0,i] = sqrt(3)*uv[0]/2
+            x[1,i] = -uv[0]/2 + uv[1]
+        elif (r > 1/3 and r <= 2/3):
+            x[0,i] = -sqrt(3)*uv[0]/2 + sqrt(3)*uv[1]/2
+            x[1,i] = -uv[0]/2 - uv[1]/2
+        else:
+            x[0,i] = -sqrt(3)*uv[1]/2
+            x[1,i] = uv[0] - uv[1]/2
+
+    x *= size
+    return x
+
+def distances(nbr_users, size=500, height=35):
+    user_pos = hexagon(nbr_users)
+    lengths = sqrt(user_pos[0,:]**2 +  user_pos[1,:]**2 )
+    return sqrt(height**2 + lengths**2)
+
+def largeScaleCoefficents(nbr_users):
+    kb = 1.38*10**(-23)
+    T = 300 # Kelvin
+    n0 = kb*T
+    # @TODO 
+    
